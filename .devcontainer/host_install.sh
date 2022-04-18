691bd11a87ec4d2b7e6520576ce4baa3c75dcfcb
Jesse Torres <20848952+JesseWebDotCom@users.noreply.github.com>
2022-04-18 12:49:11 -0400

    Added fira coda install and fallback fonts

--- .devcontainer/host_install.sh
+++ .devcontainer/host_install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 # run this script on the OSX system running vs code & docker desktop
 
-# download and install required fonts
 font_dir="$HOME/Library/Fonts"
 
-# download and install the font
+# download and install the terminal font
 # - font download links - https://www.nerdfonts.com/font-downloads
 curl -o /$HOME/meslo.zip -LO "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip"
 unzip -o /$HOME/meslo.zip -d "$font_dir"
 rm -f /$HOME/meslo.zip
+
+# download and install the code window font
+curl -o /$HOME/fira.zip -LO https://github.com/tonsky/FiraCode/releases/download/6.2/Fira_Code_v6.2.zip
+unzip -o /$HOME/fira.zip -d "/$HOME/fira"
+cp /$HOME/fira/ttf/* "$font_dir"
+rm -f /$HOME/fira.zip
+rm -rf "/$HOME/fira"
