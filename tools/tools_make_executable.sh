0a5832f4d08a622b47b19cf15e29137643969db6
Jesse Torres <20848952+JesseWebDotCom@users.noreply.github.com>
2022-04-24 19:32:18 +0000

    shellcheck compliance

--- tools/tools_make_executable.sh
+++ tools/tools_make_executable.sh
@@ -2,12 +2,13 @@
 # makes all tool scripts executable
 
 # imports
-SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit; pwd -P )"
+# shellcheck disable=SC1091
 source "$SCRIPT_DIR/common/logger.sh"
 
 # main
 out_console "Making executable..."
-chmod +x $(find . -name '*.sh')
+chmod +x "$(find . -name '*.sh')"
 
 out_console "Finished - Elapsed Time: $SECONDS second(s)"
 
