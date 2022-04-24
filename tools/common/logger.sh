4dbcf02b8bfa4096e73a0b991d6a04cd78e67a0d
JesseWebDotCom <20848952+JesseWebDotCom@users.noreply.github.com>
2022-04-24 01:51:16 -0400

    Ensure unix line feeds

--- tools/common/logger.sh
+++ tools/common/logger.sh
@@ -14,12 +14,12 @@
 #######################################
 out_console () {
     local color_reset color_bold color_red color_green color_yellow color_cyan
-    color_reset=$(tput sgr0)
-    color_bold=$(tput bold)
-    color_red=$(tput setaf 1)
-    color_green=$(tput setaf 2)
-    color_yellow=$(tput setaf 3)
-    color_cyan=$(tput setaf 6)
+    # color_reset=$(tput sgr0)
+    # color_bold=$(tput bold)
+    # color_red=$(tput setaf 1)
+    # color_green=$(tput setaf 2)
+    # color_yellow=$(tput setaf 3)
+    # color_cyan=$(tput setaf 6)
 
     local level color message caller
     level=$(echo "$2" | tr '[:lower:]' '[:upper:]')
@@ -29,17 +29,17 @@ out_console () {
 
     if [ "$level" = "ERROR" ]; then
         level="[ERROR]"
-        color=$color_bold$color_red
+        # color=$color_bold$color_red
         # prefix="[${BASH_SOURCE[1]}:${FUNCNAME[0]}] "
     elif [ "$level" = "WARN" ]; then
         level="[WARN] "
-        color=$color_yellow
+        # color=$color_yellow
     elif [ "$level" = "DEBUG" ]; then
         level="[DEBUG]"
-        color=$color_cyan
+        # color=$color_cyan
     else
         level="[INFO] "
-        color=$color_green
+        # color=$color_green
     fi
 
     printf "%s %s%s%s %s\t%s%s\n" "$(date +"%T.%2N")" "${color}" "${level}" "${color_reset}" "$caller" "${prefix}" "${message}"
